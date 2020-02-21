de2b30b847481082721994779a977db6c1505f5f
Eduardo <egarcia9650@misena.edu.co>
2020-02-21 09:59:57 -0500

    commitdesarrollo3

--- ejerciciopractica2/ejerciciophp4.php
+++ ejerciciopractica2/ejerciciophp4.php
@@ -5,7 +5,7 @@
 </head>
 
 <body>
-<form method="post" action="pagina1.php">
+<form method="post" action="pagina4.php">
     Ingrese Primer valor:
     <input type="text" name="valor1">
     <br>
---
+++ ejerciciopractica2/pagina4.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+
+</head>
+<body>
+<?php
+
+if ($_REQUEST['radio1'] == "suma") {
+    $suma = $_REQUEST['valor1'] + $_REQUEST['valor2'];
+    echo "La suma es:" . $suma;
+} else {
+    if ($_REQUEST['radio1'] == "resta") {
+        $resta = $_REQUEST['valor1'] - $_REQUEST['valor2'];
+        echo "La resta es:" . $resta;
+    }
+}
+
+?>
+
+
+</body>
+
+
+
+
+
+
+</html>
