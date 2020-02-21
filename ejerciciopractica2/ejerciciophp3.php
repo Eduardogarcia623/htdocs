650ca24a1d85d7ed5fafcaf99c5ea436395d7b5a
Eduardo <egarcia9650@misena.edu.co>
2020-02-21 09:55:39 -0500

    commitdesarrollo3

--- ejerciciopractica2/ejerciciophp3.php
+++ ejerciciopractica2/ejerciciophp3.php
@@ -1,15 +1,23 @@
 <html>
 <head>
     <title>
-        ejercicio digitos
-
     </title>
 
 </head>
-
-
-
-
-
-
+<body>
+<?php
+$valor = rand (1,100);
+echo "El valor sorteado es $valor <br>";
+if ($valor <=10)
+    { echo "Tiene un solo Digito";}
+   else{
+       if ( $valor < 100 ) {
+           echo "tiene dos digitos";
+       }
+       else{
+           echo "tiendo tres digitos";  }
+   }
+?>
+</body>
 </html>
+
---
+++ ejerciciopractica2/ejerciciophp4.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <title>Formulario de entrada del dato</title>
+</head>
+
+<body>
+<form method="post" action="pagina1.php">
+    Ingrese Primer valor:
+    <input type="text" name="valor1">
+    <br>
+    Ingrese segundo Valor:
+    <input type="text" name = "valor2">
+    <br>
+    <input type="radio" name="radio1" value="suma" >sumar
+    <br>
+    <input type="radio" name="radio1" value="resta" >restar
+    <br>
+    <input type="submit" name="operar">
+</form>
+
+</form>
+</body>
+
+</html>
