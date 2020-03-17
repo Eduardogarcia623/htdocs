c0929c0cc2cea2baea850c3904e4f0c90f4f0b2d
Eduardo <egarcia9650@misena.edu.co>
2020-03-17 09:24:05 -0500

    TECHNET9

--- technet/app/Http/Controllers/PersonaController.php
+++ technet/app/Http/Controllers/PersonaController.php
@@ -10,6 +10,7 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use App\Persona;
 
 
+
 class PersonaController extends Controller{
 
     public function index()
@@ -20,9 +21,18 @@ class PersonaController extends Controller{
     }
    public function ingreso()
    {
+       $persona = new persona();
 
-   }
+       $persona -> nombre_usuario = $_REQUEST -> $nombre_usuario;
+       $persona -> contrasena = $_REQUEST -> $contrasena;
 
+       if (Auth::attempt(['nombre_usuario' => $nombre_usuario, 'contrasena' => $contrasena, ])) {
+           // The user is active, not suspended, and exists.
+           return redirect()->intended('login');
+       }
 
 }
+
+}
+
 
--- technet/routes/web.php
+++ technet/routes/web.php
@@ -8,7 +8,11 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/','personaController@index');
-Route::get('/admin','personaController@ingreso');
+
+
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 
 
